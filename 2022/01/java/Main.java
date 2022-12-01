82e9d05cd04d0c6cdce307f18e205446f75f1da8
Christian <christian.huang.49@gmail.com>
2022-12-01 11:09:09 +0100

    feat: day01 resolve part2

--- 2022/01/java/Main.java
+++ 2022/01/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -22,25 +25,33 @@ public class Main {
         return elves;
     }
 
-    public static void resolvePartOne(List<Elf> elves) {
-        Integer max = 0;
-        for (Elf tmp : elves) {
-            if (max < tmp.sumValues()) {
-                max = tmp.sumValues();
+    public static Elf resolvePartOne(List<Elf> elves) {
+        Elf max_elf = elves.get(0);
+        for (Elf elf : elves) {
+            if (max_elf.sumValues() < elf.sumValues()) {
+                max_elf = elf;
             }
         }
-        System.out.println(max);
+        System.out.println(max_elf.sumValues());
+        return max_elf;
     }
 
     public static void resolvePartTwo(List<Elf> elves) {
+        Integer index = 0;
+        Integer result = 0;
 
-        Integer max = 0;
-
+        elves.sort(Comparator.comparing(Elf::sumValues).reversed());
+        while (index < 3) {
+            result += elves.get(index).sumValues();
+            index++;
+        }
+        System.out.println(result);
     }
     public static void main(String[] args) throws IOException {
         String[] array = Input.input("first_part.txt");
         List<Elf> elves = getElfs(array);
 
-        resolvePartOne(elves);
+//        resolvePartOne(elves);
+        resolvePartTwo(elves);
     }
 }
